25bda98716d73ce5d486ea3c1f83750453bc492b
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 19:15:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,7 +4,7 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  two_elements_array = ["Welcome to New York", "oranges"]
+  two_elements_array = ["Welcome to New York", "Out of The Woods"]
   return two_elements_array
 end
 

8618141d414d931127d152394c579ea7b6b78f69
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 19:13:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,10 +4,10 @@ def instantiate_new_array
 end
 
 def array_with_two_elements
-  two_elements_array = ["apples", "oranges"]
+  two_elements_array = ["Welcome to New York", "oranges"]
   return two_elements_array
 end
 
 def first_element(array)
-  return array_with_two_elements[1]
+  return array_with_two_elements[0]
 end
